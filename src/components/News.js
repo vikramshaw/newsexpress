5f95aa7959640b45a905db36ec8194ae5c74d35d
vikramshaw <shawvikram15@gmail.com>
2022-08-30 21:12:35 +0530

    Unneccessary fix

--- src/components/News.js
+++ src/components/News.js
@@ -1,27 +1,25 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
 
 export class News extends Component {
   render() {
     return (
-      <div>
-        <div className="container my-3">
-            <h1>NewsExpress - Top Headlines</h1>
-            <div className="row">
-                <div className="col-md-4">
-                    <NewsItem title="My Title" description="Mydesc"/>
-                </div>
-                <div className="col-md-4">
-                    <NewsItem title="My Title" description="Mydesc"/>
-                </div>
-                <div className="col-md-4">
-                    <NewsItem title="My Title" description="Mydesc"/>
-                </div>
-            </div>
+      <div className="container my-3">
+        <h1>NewsExpress - Top Headlines</h1>
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem title="My Title" description="Mydesc" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="My Title" description="Mydesc" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="My Title" description="Mydesc" />
+          </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default News
+export default News;
