ed669b3fb89b635edf3195f16311366645ed1634
vikramshaw <shawvikram15@gmail.com>
2022-09-03 20:30:39 +0530

    Adding Categories & propTypes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg bg-light">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
             <a className="navbar-brand" href="/">NewsExpress</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -11,12 +11,14 @@ export class Navbar extends Component {
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">Home</a>
-                  </li>
-                  <li className="nav-item">
-                  <a className="nav-link" href="/about">About</a>
-                  </li>
+                  <li className="nav-item"><a className="nav-link active" aria-current="page" href="/">Home</a></li>
+                  <li className="nav-item"><a className="nav-link" href="/about">About</a></li>
+                  <li className="nav-item"><a className="nav-link" href="/about">General</a></li> 
+                  <li className="nav-item"><a className="nav-link" href="/about">Sports</a></li>
+                  <li className="nav-item"><a className="nav-link" href="/about">Entertainment</a></li>
+                  <li className="nav-item"><a className="nav-link" href="/about">Science</a></li>
+                  <li className="nav-item"><a className="nav-link" href="/about">Business</a></li>
+                  <li className="nav-item"><a className="nav-link" href="/about">Health</a></li>
               </ul>
             </div>
         </div>
--- src/components/News.js
+++ src/components/News.js
@@ -1,9 +1,19 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
-import Spinner from './Spinner'
-
+import Spinner from './Spinner';
+import PropTypes from 'prop-types';
 
 export class News extends Component {
+
+  static defaultProps = {
+    country: "in",
+    pageSize: 8,
+  }
+
+  static propTypes= {
+    country: PropTypes.string,
+    pageSize:PropTypes.number,
+  }
 
   constructor() {
     super();
@@ -17,7 +27,7 @@ export class News extends Component {
   //This will run after render() has been returned
   async componentDidMount(){
     // console.log("cdm");
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apikey=9830904d976041d0989bf715702fc86e&page=1&pageSize=${this.props.pageSize}`;
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apikey=9830904d976041d0989bf715702fc86e&page=1&pageSize=${this.props.pageSize}`;
     this.setState({loading: true});
     let data = await fetch(url);
     let parsedData = await data.json()
@@ -31,7 +41,7 @@ export class News extends Component {
 
   handlePrevClick= async ()=> {
     console.log("prev");
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apikey=9830904d976041d0989bf715702fc86e&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apikey=9830904d976041d0989bf715702fc86e&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
     this.setState({loading: true});
     let data = await fetch(url);
     let parsedData = await data.json()
@@ -46,7 +56,7 @@ export class News extends Component {
   handleNextClick= async ()=> {
     console.log("Next");
     if(!(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize))){
-      let url = `https://newsapi.org/v2/top-headlines?country=in&apikey=9830904d976041d0989bf715702fc86e&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apikey=9830904d976041d0989bf715702fc86e&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
       this.setState({loading: true});
       let data = await fetch(url);
       let parsedData = await data.json()
