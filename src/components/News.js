b1a68ee2f15fb2f59db8ea7c4f7d40c2782df981
vikramshaw <shawvikram15@gmail.com>
2022-09-05 17:56:35 +0530

    Adding time, author and news source

--- src/components/News.js
+++ src/components/News.js
@@ -77,7 +77,8 @@ export class News extends Component {
         <div className="row mx-6">
           {!this.state.loading && this.state.articles.map ((element)=> {
             return <div className="col-md-4" key={element.url}>
-              <NewsItem title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,88):""}  urlToImage={element.urlToImage} newsUrl={element.url}/>
+              <NewsItem title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,88):""}  
+              urlToImage={element.urlToImage} newsUrl={element.url} author={element.author? element.author: "unknown"} date={element.publishedAt} source={element.source.name} />
             </div>
             })}
         </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,19 +2,40 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title, description, urlToImage, newsUrl} = this.props;
+    let {title, description, urlToImage, newsUrl, author, date, source} = this.props;
     return (
-      <div className='my-3'>
+      <div className="my-3">
         <div className="card">
-          <img src={!urlToImage?"https://image.cnbcfm.com/api/v1/image/107076423-16553187992022-06-15t182712z_1791861541_rc2isu9y23d9_rtrmadp_0_usa-fed.jpeg?v=1655318916&w=1920&h=1080":urlToImage} className="card-img-top" alt="..."/>
+          <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style= {{zIndex: '1', left: '90%'}}>{source}</span>
+          <img
+            src={
+              !urlToImage
+                ? "https://image.cnbcfm.com/api/v1/image/107076423-16553187992022-06-15t182712z_1791861541_rc2isu9y23d9_rtrmadp_0_usa-fed.jpeg?v=1655318916&w=1920&h=1080"
+                : urlToImage
+            }
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
-              <h5 className="card-title">{title}</h5>
-              <p className="card-text">{description}</p>
-              <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-sm btn-dark">Read More</a>
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text">
+              <small className="text-muted">
+                By {author} on {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a
+              href={newsUrl}
+              rel="noreferrer"
+              target="_blank"
+              className="btn btn-sm btn-dark"
+            >
+              Read More
+            </a>
           </div>
+        </div>
       </div>
-    </div>
-    )
+    );
   }
 }
 
