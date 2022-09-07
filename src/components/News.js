831ef054ea2170a1034671f787a97261facdd499
vikramshaw <shawvikram15@gmail.com>
2022-09-07 19:16:42 +0530

    Changing Class based NewsExpress components to Functional based

--- src/components/News.js
+++ src/components/News.js
@@ -1,92 +1,81 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from './Spinner';
 import PropTypes from 'prop-types';
 import InfiniteScroll from "react-infinite-scroll-component";
 
-export class News extends Component {
+const News = (props) => {
 
-  static defaultProps = {
-    country: "in",
-    pageSize: 8
-  }
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
 
-  static propTypes = {
-    country: PropTypes.string,
-    pageSize: PropTypes.number,
-  }
-
-  constructor() {
-    super();
-    // console.log("I'm a constructor");
-    this.state = {
-      articles: [],
-      loading: false,
-      page: 1,
-      totalResults: 0
-    }
-  }
-
-  async updateNews() {
-    this.props.setProgress(10);
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apikey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true });
-    this.props.setProgress(30);
+  const updateNews = async () => {
+    props.setProgress(10);
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apikey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+    setLoading(true);
     let data = await fetch(url);
-    this.props.setProgress(70);
-    let parsedData = await data.json()
-    console.log("cdm", parsedData);
-    this.setState({
-      articles: parsedData.articles,
-      totalResults: parsedData.totalResults,
-      loading: false
-    })
-    this.props.setProgress(100);
+    props.setProgress(30);
+    let parsedData = await data.json();
+    props.setProgress(70);
+    console.log(parsedData);
+    setArticles(parsedData.articles);
+    setTotalResults(parsedData.totalResults);
+    setLoading(false);
+    props.setProgress(100);
   }
 
-  async componentDidMount() {
-    this.updateNews();
-  }
+  useEffect(() => {
+    updateNews();
+    // eslint-disable-next-line
+  }, [])
 
-  fetchMoreData = async () => {
-    this.setState({ page: this.state.page + 1 });
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apikey=${this.props.apiKey}&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true });
+  const fetchMoreData = async () => {
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apikey=${props.apiKey}&page=${page + 1}&pageSize=${props.pageSize}`;
+    setPage(page + 1);
+    setLoading(true);
     let data = await fetch(url);
-    let parsedData = await data.json()
+    let parsedData = await data.json();
     console.log("cdm", parsedData);
-    this.setState({
-      //It will concat the 1st page article with the next page
-      articles: this.state.articles.concat(parsedData.articles),
-      totalResults: parsedData.totalResults,
-      loading: false
-    })
+    setArticles(articles.concat(parsedData.articles));
+    setTotalResults(parsedData.totalResults);
+    setLoading(false);
   }
 
-  render() {
-    return (
-      <>
-        <h1 className="text-center">NewsExpress - Top {this.props.category.charAt(0).toUpperCase() + this.props.category.slice(1)} Headlines</h1>
-        <InfiniteScroll
-          dataLength={this.state.articles.length}
-          next={this.fetchMoreData}
-          hasMore={this.state.articles.length !== this.state.articles.totalResults}
-          // this.state.loading && <Spinner> -- If the loading is true then it will show the spinner or else not
-          loader={this.state.loading && <Spinner />}>
-          <div className="container">
-            <div className="row mx-6">
-              {this.state.articles.map((element) => {
-                return <div className="col-md-4" key={element.url}>
-                          <NewsItem title={element.title ? element.title.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 88) : ""}
-                            urlToImage={element.urlToImage} newsUrl={element.url} author={element.author ? element.author : "unknown"} date={element.publishedAt} source={element.source.name} />
-                        </div>
-              })}
-            </div>
+  return (
+    <>
+      <h1 className="text-center" style={{ marginTop: '70px' }}>NewsExpress - Top {props.category.charAt(0).toUpperCase() + props.category.slice(1)} Headlines</h1>
+      <InfiniteScroll
+        dataLength={articles.length}
+        next={fetchMoreData}
+        hasMore={articles.length !== articles.totalResults}
+
+        // loading && <Spinner> -- If the loading is true then it will show the spinner or else not
+        loader={loading && <Spinner />}>
+        <div className="container">
+          <div className="row mx-6">
+            {articles.map((element) => {
+              return <div className="col-md-4" key={element.url}>
+                <NewsItem title={element.title ? element.title.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 88) : ""}
+                  urlToImage={element.urlToImage} newsUrl={element.url} author={element.author ? element.author : "unknown"} date={element.publishedAt} source={element.source.name} />
+              </div>
+            })}
           </div>
-        </InfiniteScroll>
-      </>
-    );
-  }
+        </div>
+      </InfiniteScroll>
+    </>
+  );
+}
+
+News.defaultProps = {
+  country: "in",
+  pageSize: 8
+}
+
+News.propTypes = {
+  country: PropTypes.string,
+  pageSize: PropTypes.number
 }
 
 export default News;
